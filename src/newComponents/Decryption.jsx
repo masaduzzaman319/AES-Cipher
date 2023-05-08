209a03464ba5835b068c2c58a8d0a8d8ceadee15
masaduzzaman319 <74820206+masaduzzaman319@users.noreply.github.com>
2023-05-08 10:13:46 +0600

    AES-128

--- src/newComponents/Decryption.jsx
+++ src/newComponents/Decryption.jsx
@@ -13,30 +13,46 @@ import CryptoJS from "crypto-js";
 export default function Decryption() {
   const [ciphertext, setCiphertext] = useState("");
   const [key, setKey] = useState("");
-  const [decryptedtext, setDecryptedtext] = useState("");
+  const [decryptedText, setDecryptedText] = useState("");
+  const [showKeyLengthPopup, setShowKeyLengthPopup] = useState(false);
+  const [showCopySuccess, setShowCopySuccess] = useState(false);
 
   const handleCiphertextChange = (event) => {
     setCiphertext(event.target.value);
   };
 
   const handleKeyChange = (event) => {
-    setKey(event.target.value);
+    const enteredKey = event.target.value;
+    setKey(enteredKey);
+
+    if (enteredKey.length !== 16) {
+      setShowKeyLengthPopup(true);
+    } else {
+      setShowKeyLengthPopup(false);
+    }
   };
 
   const handleDecryptClick = () => {
-    const bytes = CryptoJS.AES.decrypt(ciphertext, key);
-    const decryptedtext = bytes.toString(CryptoJS.enc.Utf8);
-    setDecryptedtext(decryptedtext);
+    const keySize = 128;
+    const keyBytes = CryptoJS.enc.Utf8.parse(key);
+    const decrypted = CryptoJS.AES.decrypt(ciphertext, keyBytes, {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+      keySize: keySize / 8,
+    }).toString(CryptoJS.enc.Utf8);
+    setDecryptedText(decrypted);
   };
 
   const handleCopyClick = () => {
-    navigator.clipboard.writeText(ciphertext);
+    navigator.clipboard.writeText(decryptedText);
+    setShowCopySuccess(true);
   };
 
   const handleClearClick = () => {
-    setDecryptedtext("");
+    setDecryptedText("");
     setKey("");
     setCiphertext("");
+    setShowCopySuccess(false);
   };
 
   return (
@@ -56,10 +72,15 @@ export default function Decryption() {
           variant="outlined"
           margin="normal"
         />
+          {showKeyLengthPopup && (
+          <Typography variant="body2" color="error">
+            The key size should be 16 characters long.
+          </Typography>
+        )}
         <Button
           sx={{
             m: "auto",
-            width: "50%",
+            width: "70%",
             borderRadius: "20px",
             fontSize: "1.2rem",
           }}
@@ -68,27 +89,30 @@ export default function Decryption() {
         >
           Decrypt
         </Button>
-
-        <Stack>
-          <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-            Decrypted text:
-          </Typography>
-          <Stack direction="row" alignItems="center">
-            <TextField
-              label="Plaintext"
-              value={decryptedtext}
-              variant="outlined"
-              margin="normal"
-              sx={{ fontSize: "1.2rem", flexGrow: 1 }}
-            />
-            {decryptedtext && (
-              <IconButton aria-label="copy" onClick={handleCopyClick}>
-                <CopyIcon />
-              </IconButton>
-            )}
-          </Stack>
+        <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+          Decrypted Text:
+        </Typography>
+        <Stack direction="row" alignItems="center">
+          <TextField
+            label="Plaintext"
+            value={decryptedText}
+            variant="outlined"
+            margin="normal"
+            sx={{ fontSize: "1.2rem", flexGrow: 1 }}
+          />
+          {decryptedText && (
+            <IconButton aria-label="copy" onClick={handleCopyClick}>
+              <CopyIcon />
+            </IconButton>
+          )}
         </Stack>
+        {showCopySuccess ? (
+  <Typography variant="subtitle1" align="center" color="success" p={2}>
+    Text copied successfully!
+  </Typography>        
+): null }
       </Stack>
+
       <Stack my={3} alignItems="center" sx={{ width: "100%" }}>
         <Button
           variant="outlined"
--- src/newComponents/Encryption.jsx
+++ src/newComponents/Encryption.jsx
@@ -1,42 +1,52 @@
 import React, { useState } from "react";
 import CopyIcon from "@mui/icons-material/FileCopy";
-
-import {
-  Box,
-  Stack,
-  TextField,
-  Button,
-  Typography,
-  IconButton,
-} from "@mui/material";
+import { Box, Stack, TextField, Button, Typography, IconButton } from "@mui/material";
 import CryptoJS from "crypto-js";
 
 export default function Encryption() {
   const [plaintext, setPlaintext] = useState("");
   const [key, setKey] = useState("");
   const [ciphertext, setCiphertext] = useState("");
+  const [showKeyLengthPopup, setShowKeyLengthPopup] = useState(false);
+  const [showCopySuccess, setShowCopySuccess] = useState(false);
 
   const handlePlaintextChange = (event) => {
     setPlaintext(event.target.value);
   };
 
   const handleKeyChange = (event) => {
-    setKey(event.target.value);
+    const enteredKey = event.target.value;
+    setKey(enteredKey);
+
+    if (enteredKey.length !== 16) {
+      setShowKeyLengthPopup(true);
+    } else {
+      setShowKeyLengthPopup(false);
+    }
   };
 
   const handleEncryptClick = () => {
-    const ciphertext = CryptoJS.AES.encrypt(plaintext, key).toString();
-    setCiphertext(ciphertext);
+    const keySize = 128;
+    const keyBytes = CryptoJS.enc.Utf8.parse(key);
+    const encrypted = CryptoJS.AES.encrypt(plaintext, keyBytes, {
+      mode: CryptoJS.mode.ECB,
+      padding: CryptoJS.pad.Pkcs7,
+      keySize: keySize / 8,
+    }).toString();
+    setCiphertext(encrypted);
   };
 
   const handleCopyClick = () => {
     navigator.clipboard.writeText(ciphertext);
+    setShowCopySuccess(true);
   };
+  
 
   const handleClearClick = () => {
     setPlaintext("");
     setKey("");
     setCiphertext("");
+    setShowCopySuccess(false);
   };
 
   return (
@@ -56,6 +66,11 @@ export default function Encryption() {
           variant="outlined"
           margin="normal"
         />
+          {showKeyLengthPopup && (
+          <Typography variant="body2" color="error">
+            The key size should be 16 characters long.
+          </Typography>
+        )}
         <Button
           sx={{
             m: "auto",
@@ -85,6 +100,12 @@ export default function Encryption() {
             </IconButton>
           )}
         </Stack>
+        {showCopySuccess ? (
+  <Typography variant="subtitle1" align="center" color="success" p={2}>
+    Text copied successfully!
+  </Typography>        
+): null }
+
       </Stack>
 
       <Stack my={3} alignItems="center" sx={{ width: "100%" }}>
